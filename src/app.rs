a892179e48dc56998db2a553516bc8308493883f
Rudo2204 <rudo2204@gmail.com>
2025-03-11 00:15:27 +0700

    Add initial opentelemetry setup

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,65 @@
 use async_trait::async_trait;
 use loco_rs::{
+    Result,
     app::{AppContext, Hooks, Initializer},
     bgworker::{BackgroundWorker, Queue},
-    boot::{create_app, BootResult, StartMode},
+    boot::{BootResult, StartMode, create_app},
     config::Config,
     controller::AppRoutes,
+    controller::middleware::{self, MiddlewareLayer},
     db::{self, truncate_table},
     environment::Environment,
     task::Tasks,
-    Result,
 };
 use migration::Migrator;
 use std::path::Path;
+use std::sync::OnceLock;
+use tracing_subscriber::{EnvFilter, Layer, layer::SubscriberExt, util::SubscriberInitExt};
+
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_otlp::LogExporter;
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::logs::SdkLoggerProvider;
 
 #[allow(unused_imports)]
 use crate::{
     controllers, initializers, models::_entities::users, tasks, workers::downloader::DownloadWorker,
 };
+
+const MODULE_WHITELIST: &[&str] = &[
+    env!("CARGO_CRATE_NAME"),
+    "loco_rs",
+    "sea_orm_migration",
+    "tower_http",
+    "sqlx::query",
+    "sidekiq",
+    "playground",
+    "loco_gen",
+    "foo",
+];
+
+fn get_resource() -> Resource {
+    static RESOURCE: OnceLock<Resource> = OnceLock::new();
+    RESOURCE
+        .get_or_init(|| {
+            Resource::builder()
+                .with_service_name(env!("CARGO_CRATE_NAME"))
+                .build()
+        })
+        .clone()
+}
+
+fn init_opentelemetry_logger() -> SdkLoggerProvider {
+    let exporter = LogExporter::builder()
+        .with_tonic()
+        .build()
+        .expect("Failed to create log exporter");
+
+    SdkLoggerProvider::builder()
+        .with_resource(get_resource())
+        .with_batch_exporter(exporter)
+        .build()
+}
 
 pub struct App;
 #[async_trait]
@@ -44,9 +87,52 @@ impl Hooks for App {
     }
 
     async fn initializers(_ctx: &AppContext) -> Result<Vec<Box<dyn Initializer>>> {
-        Ok(vec![Box::new(
-            initializers::view_engine::ViewEngineInitializer,
-        )])
+        Ok(vec![
+            Box::new(initializers::opentelemetry::OpenTelemetryInitializer),
+            Box::new(initializers::view_engine::ViewEngineInitializer),
+        ])
+    }
+
+    fn middlewares(ctx: &AppContext) -> Vec<Box<dyn MiddlewareLayer>> {
+        middleware::default_middleware_stack(ctx)
+    }
+
+    fn init_logger(config: &Config, _env: &Environment) -> Result<bool> {
+        let log_level = &config.logger.level;
+        let logger_provider = init_opentelemetry_logger();
+
+        let otel_layer = OpenTelemetryTracingBridge::new(&logger_provider);
+
+        let filter_otel = EnvFilter::new(log_level.to_string())
+            .add_directive("hyper=off".parse().unwrap())
+            .add_directive("opentelemetry=off".parse().unwrap())
+            .add_directive("tonic=off".parse().unwrap())
+            .add_directive("h2=off".parse().unwrap())
+            .add_directive("reqwest=off".parse().unwrap());
+        let otel_layer = otel_layer.with_filter(filter_otel);
+
+        let filter_fmt = EnvFilter::new(log_level.to_string())
+            .add_directive("opentelemetry=debug".parse().unwrap());
+        let fmt_layer = tracing_subscriber::fmt::layer()
+            .with_thread_names(true)
+            .with_filter(filter_fmt);
+
+        let api_fmt = EnvFilter::new(
+            MODULE_WHITELIST
+                .iter()
+                .map(|m| format!("{m}={log_level}"))
+                .collect::<Vec<_>>()
+                .join(","),
+        );
+
+        tracing_subscriber::registry()
+            .with(api_fmt)
+            .with(otel_layer)
+            .with(fmt_layer)
+            .init();
+
+        tracing::info!("Opentelemetry logger initialized");
+        Ok(true)
     }
 
     fn routes(_ctx: &AppContext) -> AppRoutes {
