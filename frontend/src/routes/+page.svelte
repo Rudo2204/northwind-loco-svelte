36e55e033a6faad32a4a325c6e2f2b1311445959
Rudo2204 <rudo2204@gmail.com>
2025-03-15 18:22:15 +0700

    Initial frontend setup

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn btn-primary">Button</button>
+<button class="btn">Button</button>
+<h1 class="text-3xl font-bold italic underline">
+  Hello world!
+</h1>
