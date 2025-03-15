1eba130e430bce9c0a51f2bf3ea5ce51b193b00b
Rudo2204 <rudo2204@gmail.com>
2025-03-15 20:26:08 +0700

    Reformat with prettier

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,4 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <button class="btn btn-primary">Button</button>
 <button class="btn">Button</button>
-<h1 class="text-3xl font-bold italic underline">
-  Hello world!
-</h1>
+<h1 class="text-3xl font-bold italic underline">Hello world!</h1>
