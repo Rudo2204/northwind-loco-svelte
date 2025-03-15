b230fad5c2944e4c0d442244d0bd0c20f0bc42af
Rudo2204 <rudo2204@gmail.com>
2025-03-15 13:22:41 +0700

    Add default PaginationResponse

---
+++ src/views/utils.rs
@@ -0,0 +1,27 @@
+use loco_rs::{
+    controller::views::pagination::{Pager, PagerMeta},
+    model::query::{PageResponse, PaginationQuery},
+};
+use sea_orm::ModelTrait;
+use serde::{Deserialize, Serialize};
+
+/// Default PaginationResponse
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PaginationResponse {}
+
+impl PaginationResponse {
+    #[must_use]
+    pub fn response<T: ModelTrait>(
+        data: PageResponse<T>,
+        pagination_query: &PaginationQuery,
+    ) -> Pager<Vec<T>> {
+        Pager {
+            results: data.page,
+            info: PagerMeta {
+                page: pagination_query.page,
+                page_size: pagination_query.page_size,
+                total_pages: data.total_pages,
+            },
+        }
+    }
+}
