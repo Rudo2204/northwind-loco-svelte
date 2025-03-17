5428c4bd6c38a0908f2bce0af2e6710c314f7c91
Rudo2204 <rudo2204@gmail.com>
2025-03-17 16:45:54 +0700

    Add navigation components

---
+++ frontend/src/lib/components/navigation/Navbar.svelte
@@ -0,0 +1,36 @@
+<div class="navbar bg-base-200 shadow-sm">
+  <div class="flex-1">
+    <div class="text-2xl">
+      <a href="/">
+        <span class="font-black">Northwind</span> Traders
+      </a>
+    </div>
+  </div>
+  <div class="flex gap-2">
+    <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+
+      <ul class="menu menu-md dropdown-content bg-base-200 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>About</a></li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
---
+++ frontend/src/lib/components/navigation/Sidebar.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+  import {
+    ArrowLongLeftIcon,
+    Bar3Icon,
+    HomeIcon,
+    IdentificationIcon,
+    InboxStackIcon,
+    ShoppingBagIcon,
+    ShoppingCartIcon,
+    UsersIcon
+  } from '../icons';
+  let isSidebarExpanded = $state(true);
+  let { children } = $props();
+</script>
+
+<div class="drawer drawer-open">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content m-12">
+    {@render children()}
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+    <aside
+      class={`menu bg-base-200 text-base-content min-h-full p-4 ${isSidebarExpanded ? 'w-60' : ''}`}
+    >
+      <div class={`flex ${isSidebarExpanded ? 'justify-end' : 'justify-center'}`}>
+        <button
+          class="btn btn-ghost btn-square"
+          onclick={() => (isSidebarExpanded = !isSidebarExpanded)}
+        >
+          {#if isSidebarExpanded}
+            <ArrowLongLeftIcon />
+          {:else}
+            <Bar3Icon />
+          {/if}
+        </button>
+      </div>
+      <ul>
+        <li>
+          <a href="/">
+            <HomeIcon />
+            {#if isSidebarExpanded}
+              Home
+            {/if}
+          </a>
+        </li>
+        <div class="divider m-0"></div>
+        <li>
+          <a>
+            <InboxStackIcon />
+            {#if isSidebarExpanded}
+              Suppliers
+            {/if}
+          </a>
+        </li>
+        <li>
+          <a>
+            <ShoppingCartIcon />
+            {#if isSidebarExpanded}
+              Products
+            {/if}
+          </a>
+        </li>
+        <li>
+          <a>
+            <ShoppingBagIcon />
+            {#if isSidebarExpanded}
+              Orders
+            {/if}
+          </a>
+        </li>
+        <li>
+          <a href="/employees">
+            <IdentificationIcon />
+            {#if isSidebarExpanded}
+              Employees
+            {/if}
+          </a>
+        </li>
+        <li>
+          <a>
+            <UsersIcon />
+            {#if isSidebarExpanded}
+              Customers
+            {/if}
+          </a>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</div>
