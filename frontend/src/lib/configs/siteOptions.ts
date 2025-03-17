e442bee12ffe9fe568290f3735eb88dcfa4d3d67
Rudo2204 <rudo2204@gmail.com>
2025-03-17 16:34:54 +0700

    Add site configs

---
+++ frontend/src/lib/configs/siteOptions.ts
@@ -0,0 +1,4 @@
+export abstract class SiteOptions {
+  static page: number = 1;
+  static pageSize: number = 15;
+}
---
+++ frontend/src/lib/configs/website.ts
@@ -0,0 +1,8 @@
+const website = {
+  siteTitle: 'Northwind Traders',
+  siteLanguage: 'en',
+  author: 'rudo2204',
+  siteUrl: '/'
+};
+
+export default website;
