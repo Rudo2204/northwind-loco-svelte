a6cbe31a5f6f0deff228c6949cadc2da707f8a9d
Rudo2204 <rudo2204@gmail.com>
2025-03-18 16:54:47 +0700

    Add EmployeeCollectionResponse

---
+++ frontend/src/lib/shared/responses/employees/employeeCollectionResponse.ts
@@ -0,0 +1,4 @@
+import { BaseCollectionResponse } from '../baseCollectionResponse';
+import type { EmployeeResponse } from './employeeResponse';
+
+export class EmployeeCollectionResponse extends BaseCollectionResponse<EmployeeResponse> {}
