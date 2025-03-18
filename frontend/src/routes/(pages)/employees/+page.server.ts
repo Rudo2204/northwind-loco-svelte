d3e8eaddb5a6cbb6fd93daadeab57b2049300558
Rudo2204 <rudo2204@gmail.com>
2025-03-18 18:20:22 +0700

    Establish initial pattern

--- frontend/src/routes/(pages)/employees/+page.server.ts
+++ frontend/src/routes/(pages)/employees/+page.server.ts
@@ -1,45 +1,18 @@
-import { NORTHWIND_API_URL } from '$env/static/private';
 import type { PageServerLoad } from './$types';
-
-interface PagerMeta {
-  page: number;
-  page_size: number;
-  total_pages: number;
-  total_items: number;
-}
-
-interface Employee {
-  employeeid: number;
-  created_at: Date;
-  updated_at: Date;
-  lastname: string;
-  firstname: string;
-  title: string;
-  titleofcourtesy: string;
-  birthdate: Date;
-  hiredate: Date;
-  address: string;
-  city: string;
-  region: string;
-  postalcode: string;
-  country: string;
-  homephone: string;
-  extension: string;
-  photo: Uint8Array;
-  notes: string | null;
-  reportsto: number | null;
-  photopath: string;
-}
-
-interface Employees {
-  results: Employee[];
-  pagination: PagerMeta;
-}
+import type { EmployeeCollectionResponse } from '$lib/shared/responses';
+import { PaginationQueries } from '$lib/shared/queries';
 
 export const load: PageServerLoad = async ({ fetch, url }) => {
-  const page = url.searchParams.get('page') || '1';
-  const res = await fetch(`${NORTHWIND_API_URL}/api/employees?page=${page}`);
-  const employees: Employees = await res.json();
-
+  const employees = await PageHelper.getEmployees(fetch, url.searchParams);
   return { employees };
 };
+
+abstract class PageHelper {
+  static async getEmployees(fetch: any, search?: URLSearchParams) {
+    const query = PaginationQueries.getPaginationQuery(search);
+    const response = await fetch('/api/v1/employees' + query);
+    const parsedResponse: EmployeeCollectionResponse = await response.json();
+
+    return parsedResponse;
+  }
+}
