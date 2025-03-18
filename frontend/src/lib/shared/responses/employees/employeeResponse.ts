ea8195c8d7c591f067246fc89ea3702b07e66fbb
Rudo2204 <rudo2204@gmail.com>
2025-03-18 16:14:49 +0700

    Add initial lib/shared/responses

---
+++ frontend/src/lib/shared/responses/employees/employeeResponse.ts
@@ -0,0 +1,22 @@
+export interface EmployeeResponse {
+  employeeid: number;
+  lastname: string;
+  firstname: string;
+  title: string;
+  titleofcourtesy: string;
+  birthdate: Date;
+  hiredate: Date;
+  address: string;
+  city: string;
+  region: string;
+  postalcode: string;
+  country: string;
+  homephone: string;
+  extension: string;
+  photo: Uint8Array;
+  notes: string;
+  reportsto: number | null;
+  photopath: string;
+  created_at: Date;
+  updated_at: Date;
+}
