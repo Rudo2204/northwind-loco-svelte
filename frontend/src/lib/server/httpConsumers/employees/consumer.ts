35c4118eee735d0017ceb6e55bc05961b15a3f74
Rudo2204 <rudo2204@gmail.com>
2025-03-19 19:56:48 +0700

    Extend employee consumer from base consumer

--- frontend/src/lib/server/httpConsumers/employees/consumer.ts
+++ frontend/src/lib/server/httpConsumers/employees/consumer.ts
@@ -1,18 +1,15 @@
-import type { EmployeeCollectionResponse, EmployeeResponse } from '$lib/shared/responses';
-import { NORTHWIND_API_URL } from '$env/static/private';
+import { BaseConsumer } from '../baseConsumer';
 
-export abstract class EmployeeConsumer {
-  public static async getEmployeeCollection(query?: string): Promise<EmployeeCollectionResponse> {
-    const endpoint = `${NORTHWIND_API_URL}employees${query}`;
-    const response = await fetch(endpoint);
-    const employees: EmployeeCollectionResponse = await response.json();
-    return employees;
+import type { EmployeeCollectionResponse, EmployeeResponse } from '$lib/shared/responses';
+
+export class EmployeeConsumer extends BaseConsumer {
+  listEndpoint = 'employees';
+
+  async getEmployeeCollection(query?: string) {
+    return this.getList<EmployeeCollectionResponse>(query);
   }
 
-  public static async getCharacter(employeeid: number): Promise<EmployeeResponse> {
-    const endpoint = `${NORTHWIND_API_URL}employees/${employeeid}`;
-    const response = await fetch(endpoint);
-    const employee: EmployeeResponse = await response.json();
-    return employee;
+  async getEmployee(employeeid: number) {
+    return this.getItem<EmployeeResponse>(employeeid);
   }
 }
