ea7f70d37f1584df19bc2f854893a5d7883b14a9
Rudo2204 <rudo2204@gmail.com>
2025-03-20 11:17:36 +0700

    Change function name to be more inline with standard

--- frontend/src/lib/server/httpConsumers/employees/consumer.ts
+++ frontend/src/lib/server/httpConsumers/employees/consumer.ts
@@ -5,11 +5,11 @@ import type { EmployeeCollectionResponse, EmployeeResponse } from '$lib/shared/r
 export class EmployeeConsumer extends BaseConsumer {
   listEndpoint = 'employees';
 
-  async getEmployeeCollection(query?: string) {
-    return this.getList<EmployeeCollectionResponse>(query);
+  async getList(query?: string) {
+    return this.baseGetList<EmployeeCollectionResponse>(query);
   }
 
-  async getEmployee(employeeid: number) {
-    return this.getItem<EmployeeResponse>(employeeid);
+  async getOne(employeeid: number) {
+    return this.baseGetOne<EmployeeResponse>(employeeid);
   }
 }
