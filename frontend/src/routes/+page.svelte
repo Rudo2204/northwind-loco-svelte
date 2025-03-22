e1467feaf392a1e4d16dd4a34a58c30a0f3bd30a
Rudo2204 <rudo2204@gmail.com>
2025-03-22 19:58:54 +0700

    Add some buttons to test theme

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<button class="btn btn-primary">Button</button>
-<button class="btn">Button</button>
+<button class="btn">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>
 <h1 class="text-3xl font-bold italic underline">Hello world!</h1>
