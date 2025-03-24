be63d0b014d781b3a1907ee10c78a1cb4274cf97
Rudo2204 <rudo2204@gmail.com>
2025-03-24 17:12:14 +0700

    Put table in a card

--- frontend/src/lib/components/table/Table.svelte
+++ frontend/src/lib/components/table/Table.svelte
@@ -1,36 +1,42 @@
 <script lang="ts">
   import Pagination from './Pagination.svelte';
-  let { data, header, row } = $props();
+  let { data, title, header, row } = $props();
 
   const capitalize = (string: string) => {
     return string ? string.charAt(0).toUpperCase() + string.slice(1).toLowerCase() : '';
   };
 </script>
 
-<div>
-  <div class="border-base-300 overflow-x-auto border-b-1">
-    <table class="table">
-      {#if header}
-        <thead class="bg-base-300">
-          <tr>
-            {#each header as h}
-              <th>{capitalize(h)}</th>
-            {/each}
-          </tr>
-        </thead>
-      {/if}
+<div class="card bg-base-100 border-base-300 w-full border-t shadow-sm">
+  <div class="card-body">
+    {#if title}
+      <h2 class="card-title">{title}</h2>
+    {/if}
 
-      <tbody>
-        {#each data.results as d}
-          <tr class="hover:bg-primary/10">
-            {@render row(d)}
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </div>
+    <div class="border-base-300 overflow-x-auto border-b-1">
+      <table class="table">
+        {#if header}
+          <thead class="bg-base-300">
+            <tr>
+              {#each header as h}
+                <th>{capitalize(h)}</th>
+              {/each}
+            </tr>
+          </thead>
+        {/if}
 
-  <div class="flex justify-end">
-    <Pagination paginationData={data.pagination} />
+        <tbody>
+          {#each data.results as d}
+            <tr class="hover:bg-primary/10">
+              {@render row(d)}
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+
+    <div class="flex justify-end">
+      <Pagination paginationData={data.pagination} />
+    </div>
   </div>
 </div>
