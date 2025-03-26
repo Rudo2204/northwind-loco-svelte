ba20148d720d0bd20ddb124eeb896911a460b2bf
Rudo2204 <rudo2204@gmail.com>
2025-03-26 21:41:12 +0700

    Add shippers controller

---
+++ src/controllers/shippers.rs
@@ -0,0 +1,37 @@
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::unnecessary_struct_initialization)]
+#![allow(clippy::unused_async)]
+use axum::debug_handler;
+use axum::extract::{Path, Query};
+use loco_rs::prelude::*;
+
+use crate::controllers::load_item;
+use crate::models::_entities::shippers;
+use crate::views::PaginationResponse;
+
+#[debug_handler]
+#[tracing::instrument(skip(ctx))]
+pub async fn list(
+    query: Query<query::PaginationQuery>,
+    State(ctx): State<AppContext>,
+) -> Result<impl IntoResponse> {
+    let res = query::fetch_page(&ctx.db, shippers::Entity::find(), &query).await?;
+    Ok(format::json(PaginationResponse::response(res, &query)))
+}
+
+#[debug_handler]
+#[tracing::instrument(skip(ctx))]
+pub async fn get_one(
+    Path(id): Path<i32>,
+    State(ctx): State<AppContext>,
+) -> Result<impl IntoResponse> {
+    let item = load_item::<shippers::Entity>(&ctx, id).await?;
+    format::json(item)
+}
+
+pub fn routes() -> Routes {
+    Routes::new()
+        .prefix("api/shippers")
+        .add("/", get(list))
+        .add("/{id}", get(get_one))
+}
