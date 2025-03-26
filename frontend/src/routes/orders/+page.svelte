7c600185fddea7e1a388a1dcd282f69b85504764
Rudo2204 <rudo2204@gmail.com>
2025-03-26 20:46:23 +0700

    Add missing table columns

--- frontend/src/routes/orders/+page.svelte
+++ frontend/src/routes/orders/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   import { SeoHead, ListTable } from '$lib/components';
-  import type { OrderResponse } from '$lib/shared/responses';
+  import type { TransformedOrderResponse } from '$lib/shared/responses';
   import type { PageData } from './$types';
   const { data }: { data: PageData } = $props();
-  //header={['ID', 'Total Price', 'Products', 'Quantity', 'Shipped', 'Ship Name', 'City', 'Country']}
 </script>
 
-{#snippet row(order: OrderResponse)}
+{#snippet row(order: TransformedOrderResponse)}
   <td class="link link-primary">
     <a href="/orders/{order.orderid}">
       {order.orderid}
     </a>
   </td>
+  <td>${order.totalprice}</td>
+  <td>{order.totalproducts}</td>
+  <td>{order.totalquantity}</td>
   <td>{order.shippeddate || '-'}</td>
   <td>{order.shipname}</td>
   <td>{order.shipcity}</td>
@@ -22,6 +24,6 @@
 <ListTable
   data={data.orders}
   title="Orders"
-  header={['ID', 'Shipped', 'Ship Name', 'City', 'Country']}
+  header={['ID', 'Total Price', 'Products', 'Quantity', 'Shipped', 'Ship Name', 'City', 'Country']}
   {row}
 />
