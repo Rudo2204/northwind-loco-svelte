b1767aa2f6243523c4836104174e04c4ccce0e5e
Rudo2204 <rudo2204@gmail.com>
2025-03-26 21:37:44 +0700

    Change get_one of customers to find by customer id

--- src/controllers/customers.rs
+++ src/controllers/customers.rs
@@ -5,9 +5,13 @@ use axum::debug_handler;
 use axum::extract::{Path, Query};
 use loco_rs::prelude::*;
 
-use crate::controllers::load_item;
 use crate::models::_entities::customers;
 use crate::views::PaginationResponse;
+
+async fn load_item(ctx: &AppContext, customerid: &str) -> Result<customers::Model> {
+    let item = customers::Model::find_by_customerid(&ctx.db, customerid).await?;
+    Ok(item)
+}
 
 #[debug_handler]
 #[tracing::instrument(skip(ctx))]
@@ -22,10 +26,10 @@ pub async fn list(
 #[debug_handler]
 #[tracing::instrument(skip(ctx))]
 pub async fn get_one(
-    Path(id): Path<i32>,
+    Path(customerid): Path<String>,
     State(ctx): State<AppContext>,
 ) -> Result<impl IntoResponse> {
-    let item = load_item::<customers::Entity>(&ctx, id).await?;
+    let item = load_item(&ctx, customerid.as_str()).await?;
     format::json(item)
 }
 
@@ -33,5 +37,5 @@ pub fn routes() -> Routes {
     Routes::new()
         .prefix("api/customers")
         .add("/", get(list))
-        .add("/{id}", get(get_one))
+        .add("/{customerid}", get(get_one))
 }
