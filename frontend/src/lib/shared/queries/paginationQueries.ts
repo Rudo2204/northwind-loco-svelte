34d844d4b6d8655a04b5822ef238eb9d42cf1f36
Rudo2204 <rudo2204@gmail.com>
2025-04-07 17:08:06 +0700

    Rewrite to parse using query string

--- frontend/src/lib/shared/queries/paginationQueries.ts
+++ frontend/src/lib/shared/queries/paginationQueries.ts
@@ -1,34 +1,36 @@
 import { SiteOptions } from '$lib/configs/siteOptions';
 import type { PaginationRequest } from '$lib/shared/requests';
+import queryString from 'query-string';
+
+export function getPaginationUrlParams(search?: URLSearchParams): PaginationRequest {
+  let params: PaginationRequest;
+
+  if (search) {
+    params = {
+      page: search.get('page') ? Number(search.get('page')) : SiteOptions.page,
+      pageSize: search.get('page_size') ? Number(search.get('page_size')) : SiteOptions.pageSize
+    };
+  } else {
+    params = {
+      page: SiteOptions.page,
+      pageSize: SiteOptions.pageSize
+    };
+  }
+  return params;
+}
 
 export abstract class PaginationQueries {
-  static getPaginationQuery(search?: URLSearchParams): string {
-    let params: PaginationRequest;
+  static getPaginationQuery(search: URLSearchParams): string {
+    const parsed = queryString.parse(search.toString(), {
+      parseNumbers: true,
+      parseBooleans: true
+    });
 
-    if (search) {
-      params = {
-        page: search.get('page') ? Number(search.get('page')) : SiteOptions.page,
-        pageSize: search.get('page_size') ? Number(search.get('page_size')) : SiteOptions.pageSize
-      };
-    } else {
-      params = {
-        page: SiteOptions.page,
-        pageSize: SiteOptions.pageSize
-      };
-    }
+    const { page, pageSize } = getPaginationUrlParams(search);
+    parsed.page = page;
+    parsed.pageSize = pageSize;
 
-    const { page, pageSize } = params;
-    let urlSearchParams = new URLSearchParams();
-
-    if (page !== undefined) {
-      urlSearchParams.append('page', page.toString());
-    }
-    if (pageSize !== undefined) {
-      urlSearchParams.append('page_size', pageSize.toString());
-    }
-
-    let query = '?' + urlSearchParams.toString();
-
+    const query = '?' + queryString.stringify(parsed);
     return query;
   }
 }
