8fc026e7cdf795b26305bdff305c4537652802d2
Rudo2204 <rudo2204@gmail.com>
2025-05-25 22:47:38 +0700

    Handle operator & value change

--- frontend/src/lib/components/filters/NumberFilter.svelte
+++ frontend/src/lib/components/filters/NumberFilter.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import type { SingleFilterConfig } from '$lib/components';
+  import type { NumberOperator } from '$lib/components/filters';
   import { TrashIcon } from '$lib/components/icons';
+  import { labelMap } from './config';
+  import findIndex from 'lodash/findIndex';
 
   let {
     displayedFilters = $bindable(),
@@ -11,22 +14,29 @@
     removeFilter: (id: string) => void;
     filterItem: SingleFilterConfig;
   } = $props();
-  $inspect(displayedFilters);
+  const index = findIndex(displayedFilters, ['id', filterItem.id]);
 
   function handleOperatorChange(event: Event) {
-    const filterLabel: string = (event.target as HTMLInputElement).value;
+    const value: string = (event.target as HTMLInputElement).value;
+    const key = (Object.keys(labelMap) as Array<string>).find((key) => labelMap[key] === value);
+    displayedFilters[index] = { ...filterItem, operator: key as NumberOperator };
+  }
+
+  function handleValueChange(event: Event) {
+    const newValue: string = (event.target as HTMLInputElement).value;
+    displayedFilters[index] = { ...filterItem, value: newValue };
   }
 </script>
 
 <div class="flex pr-0 pl-0">
   <input type="text" placeholder={filterItem.source} class="input" disabled />
-  <select class="select w-20 focus:outline-none" onchange={(event) => handleOperatorChange(event)}>
+  <select class="select w-20 focus:outline-none" onchange={handleOperatorChange}>
     <option disabled selected>Op</option>
     {#each filterItem.validOperators as option}
       <option value={option.operatorLabel}>{option.operatorLabel}</option>
     {/each}
   </select>
-  <input type="text" placeholder="Value" class="input" />
+  <input type="text" placeholder="Value" class="input" onchange={handleValueChange} />
   <div class="flex flex-1"></div>
   <button class="btn btn-circle btn-ghost" onclick={() => removeFilter(filterItem.id)}>
     <TrashIcon />
